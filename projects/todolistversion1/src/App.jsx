4b85bd90af7dab380da78eca136d6140742d9eeb
hiteshkumarroy <hiteshroy011@gmail.com>
2024-07-15 02:53:01 +0530

    todoversion 1

---
+++ projects/todolistversion1/src/App.jsx
@@ -0,0 +1,21 @@
+import Entertodolist from "./components/Entertodolist";
+import List from "./components/List";
+import Title from "./components/Title";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"
+
+function App() {
+
+
+  return (
+    <>
+    <center>
+    <Title/>
+<Entertodolist/>
+<List/>
+</center>
+     </>
+  )
+}
+
+export default App
---
+++ projects/todolistversion1/src/components/List.jsx
@@ -0,0 +1,36 @@
+import "../App.css"
+export default function List(){
+  return <div>
+  <div class="container">
+   
+    <div class="row">
+      <div class="col-6">
+   drink milk
+      </div>
+      <div class="col-4">
+      <p>04/01/2024</p>
+      </div>
+      <div class="col-2">
+      <button type="button" class="btn btn-danger ">delete</button>
+      </div>
+    </div>
+  </div>
+  <div>
+  <div class="container">
+   
+    <div class="row">
+      <div class="col-6">
+   go college
+      </div>
+      <div class="col-4">
+      <p>04/01/2024</p>
+      </div>
+      <div class="col-2">
+      <button type="button" class="btn btn-danger ">delete</button>
+      </div>
+    </div>
+  </div>
+  
+    </div>
+    </div>
+}
